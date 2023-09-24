6bd69bf7b6f253ac2424a4a53ddb3846bf97cf58
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-24 16:23:06 +0300

    feat: Добавить функцию разворота строки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { reverseString } from "./algorythm";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
+import styles from "./string.module.css";
 
-console.log(reverseString("Строка текста"));
 const str = reverseString("Строка текста");
 
 export const StringComponent: React.FC = () => {
-  return <SolutionLayout title="Строка">{str}</SolutionLayout>;
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const onSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+    setIsAnimating(!isAnimating);
+    setTimeout(() => {
+      setIsAnimating((isAnimating) => !isAnimating);
+    }, 2000);
+  };
+
+  return (
+    <SolutionLayout title="Строка">
+      <form className={styles.form} onSubmit={onSubmit}>
+        <Input placeholder="Введите текст" isLimitText={true} maxLength={11} />
+        <Button type="submit" text="Развернуть" isLoader={isAnimating} />
+      </form>
+      <div className={styles.algorythm}>{str}</div>
+    </SolutionLayout>
+  );
 };
