22a62f11c2c692a2df1e4b711bfa75479a935f53
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-24 16:27:43 +0300

    feat: Добавить обработчик событий, анимацию кнопки при отправке формы

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { reverseString } from "./algorythm";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import styles from "./string.module.css";
-
-const str = reverseString("Строка текста");
+let str = "";
 
 export const StringComponent: React.FC = () => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const onSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+    setIsAnimating(!isAnimating);
+    str = reverseString("Строка текста");
+    setTimeout(() => {
+      setIsAnimating((isAnimating) => !isAnimating);
+    }, 2000);
+  };
+
   return (
     <SolutionLayout title="Строка">
-      <form className={styles.form} onSubmit={() => false}>
+      <form className={styles.form} onSubmit={onSubmit}>
         <Input placeholder="Введите текст" isLimitText={true} maxLength={11} />
-        <Button type="submit" text="Развернуть" isLoader={false} />
+        <Button type="submit" text="Развернуть" isLoader={isAnimating} />
       </form>
       <div className={styles.algorythm}>{str}</div>
     </SolutionLayout>
