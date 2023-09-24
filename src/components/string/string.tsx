0ffdae39cf986c5388c3bc6d03d3680dac123b19
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-24 16:26:05 +0300

    feat: Добавить форму для ввода текста с кнопкой

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { reverseString } from "./algorythm";
 import { Button } from "../ui/button/button";
@@ -8,21 +8,11 @@ import styles from "./string.module.css";
 const str = reverseString("Строка текста");
 
 export const StringComponent: React.FC = () => {
-  const [isAnimating, setIsAnimating] = useState(false);
-
-  const onSubmit = (evt: FormEvent) => {
-    evt.preventDefault();
-    setIsAnimating(!isAnimating);
-    setTimeout(() => {
-      setIsAnimating((isAnimating) => !isAnimating);
-    }, 2000);
-  };
-
   return (
     <SolutionLayout title="Строка">
-      <form className={styles.form} onSubmit={onSubmit}>
+      <form className={styles.form} onSubmit={() => false}>
         <Input placeholder="Введите текст" isLimitText={true} maxLength={11} />
-        <Button type="submit" text="Развернуть" isLoader={isAnimating} />
+        <Button type="submit" text="Развернуть" isLoader={false} />
       </form>
       <div className={styles.algorythm}>{str}</div>
     </SolutionLayout>
