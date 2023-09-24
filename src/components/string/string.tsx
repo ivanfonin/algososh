b15d5e8d5d877337e9e6e9d4457ae5839270b12e
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-24 16:47:10 +0300

    feat: Добавить разворот строки по клику на кнопку

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,27 +1,41 @@
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { reverseString } from "./algorythm";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import styles from "./string.module.css";
-let str = "";
 
 export const StringComponent: React.FC = () => {
   const [isAnimating, setIsAnimating] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [str, setStr] = useState("");
 
-  const onSubmit = (evt: FormEvent) => {
+  const onSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+    // В момент отправки формы создаем кружки из букв
+    // Отправляем их на сортировку
+    // Передаем в функцию сортировки setIsAnimating
+    // Передаем в функцию сортировки состояние кружков
+    console.log(inputValue);
     setIsAnimating(!isAnimating);
-    str = reverseString("Строка текста");
+    setStr(reverseString(inputValue));
     setTimeout(() => {
       setIsAnimating((isAnimating) => !isAnimating);
+      setStr(() => "");
     }, 2000);
   };
 
   return (
     <SolutionLayout title="Строка">
       <form className={styles.form} onSubmit={onSubmit}>
-        <Input placeholder="Введите текст" isLimitText={true} maxLength={11} />
+        <Input
+          onChange={(evt: FormEvent<HTMLInputElement>) =>
+            setInputValue((evt.target as HTMLInputElement).value)
+          }
+          placeholder="Введите текст"
+          isLimitText={true}
+          maxLength={11}
+        />
         <Button type="submit" text="Развернуть" isLoader={isAnimating} />
       </form>
       <div className={styles.algorythm}>{str}</div>
