af80c700efe530733e34455bad247024ce21aa58
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-25 18:12:45 +0300

    feat: Очищаем поле ввода

--- src/components/string/algorythm.ts
+++ src/components/string/algorythm.ts
@@ -1,16 +1,38 @@
 import { swap } from "../../utils/swap";
+import { pause } from "../../utils/pause";
+import { DELAY_IN_MS } from "../../constants/delays";
+import { ElementStates } from "../../types/element-states";
 
 /**
- * Функция разворачивает строку.
+ * Функция наоброт массив элементов, представляющих собой символы в строке.
  *
- * @param {String} str – Строка текста.
+ * @param {Array} letters – Массив объектов букв с состоянием.
+ * @param {Function} setCircles - Функция устанвливает стейт букв.
+ * @param {Function} setIsAnimating - Функция устанвливает стейт анимации.
  *
- * @return {String} Перевернутая строка текста.
+ * @return {void}
  */
-export const reverseString = (str: string) => {
-  const arr = str.split("");
-  for (let i = 0; i < Math.ceil(str.length / 2); i++) {
-    swap(arr, i, str.length - i - 1);
+export const reverseArray = async (
+  letters: any,
+  setCircles: any,
+  setIsAnimating: any
+) => {
+  setIsAnimating(true);
+  await pause(DELAY_IN_MS);
+  for (let i = 0; i < Math.ceil(letters.length / 2); i++) {
+    // Подсвечиваем элементы, которые меняются местами.
+    if (i !== letters.length - i - 1) {
+      letters[i].state = ElementStates.Changing;
+      letters[letters.length - i - 1].state = ElementStates.Changing;
+      setCircles([...letters]);
+    }
+    // Делаем паузу.
+    await pause(DELAY_IN_MS);
+    // Делаем перестановку и подсвечиваем элементы, которые поменяли.
+    swap(letters, i, letters.length - i - 1);
+    letters[i].state = ElementStates.Modified;
+    letters[letters.length - i - 1].state = ElementStates.Modified;
+    setCircles([...letters]);
   }
-  return arr.join("");
+  setIsAnimating(false);
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,45 +1,37 @@
 import React, { FormEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
-// import { reverseString } from "./algorythm";
+import { reverseArray } from "./algorythm";
 import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
-import { Circle, CircleProps } from "../ui/circle/circle";
+import { Circle } from "../ui/circle/circle";
 import { ElementStates } from "../../types/element-states";
 import styles from "./string.module.css";
 
-type CircleArray = Array<React.ReactElement<CircleProps>>;
+type TLetter = {
+  state: ElementStates;
+  letter: string;
+};
 
 export const StringComponent: React.FC = () => {
   const [isAnimating, setIsAnimating] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const [circles, setCircles] = useState<CircleArray>([]);
+  const [circles, setCircles] = useState<TLetter[]>([]);
 
   const onSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-
-    // В момент отправки формы создаем кружки из букв
-    setCircles(
-      inputValue
-        .split("")
-        .map((letter, i) => (
-          <Circle key={i} state={ElementStates.Default} letter={letter} />
-        ))
-    );
-
-    // Отправляем их на сортировку
-    // Передаем в функцию сортировки setIsAnimating
-    // Передаем в функцию сортировки состояние кружков circles, setCircles
-    console.log(inputValue);
-    setIsAnimating(!isAnimating);
-    setTimeout(() => {
-      setIsAnimating((isAnimating) => !isAnimating);
-    }, 2000);
+    const letters = inputValue.split("").map((letter) => ({
+      state: ElementStates.Default,
+      letter,
+    }));
+    setInputValue("");
+    reverseArray(letters, setCircles, setIsAnimating);
   };
 
   return (
     <SolutionLayout title="Строка">
       <form className={styles.form} onSubmit={onSubmit}>
         <Input
+          value={inputValue}
           name="reverse"
           placeholder="Введите текст"
           maxLength={11}
@@ -51,7 +43,9 @@ export const StringComponent: React.FC = () => {
         />
         <Button type="submit" text="Развернуть" isLoader={isAnimating} />
       </form>
-      <div className={styles.algorythm}>{circles}</div>
+      <div className={styles.algorythm}>
+        {circles && circles.map((letter, i) => <Circle key={i} {...letter} />)}
+      </div>
     </SolutionLayout>
   );
 };
