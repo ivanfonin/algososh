5d79721c67051d614028ab3b07f2cc5031eac38b
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-26 22:05:45 +0300

    feat: Принудить радио инпуты работать

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Button } from "../ui/button/button";
@@ -6,13 +6,41 @@ import { Column } from "../ui/column/column";
 import { Direction } from "../../types/direction";
 import styles from "./sorting-page.module.css";
 
+export enum Algorithms {
+  Select = "select",
+  Bubble = "bubble",
+}
+
 export const SortingPage: React.FC = () => {
+  const [isAnimating, setIsAnimating] = useState<boolean>(false);
+  const [algorithm, setAlgorithm] = useState<string>(Algorithms.Bubble);
   const [columns, setColumns] = useState([]);
+
+  const handlealgorithmSelected = (evt: ChangeEvent<HTMLInputElement>) => {
+    setAlgorithm(evt.target.value);
+  };
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.grid}>
-        <RadioInput extraClass={styles.select} label="Выбор" />
-        <RadioInput extraClass={styles.bubble} label="Пузырёк" />
+        <RadioInput
+          name="sortingAlgorithm"
+          value={Algorithms.Select}
+          checked={algorithm === Algorithms.Select}
+          onChange={handlealgorithmSelected}
+          extraClass={styles.select}
+          disabled={isAnimating}
+          label="Выбор"
+        />
+        <RadioInput
+          name="sortingAlgorithm"
+          value={Algorithms.Bubble}
+          checked={algorithm === Algorithms.Bubble}
+          onChange={handlealgorithmSelected}
+          extraClass={styles.bubble}
+          disabled={isAnimating}
+          label="Пузырёк"
+        />
         <Button
           extraClass={styles.asc}
           type="button"
