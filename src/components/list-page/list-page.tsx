074b8fe6045d17b85aab38bd1b10478601d7ad5d
Ivan Fonin <ivanfonin@yandex.ru>
2023-09-30 14:55:02 +0300

    feat: Добавить анимаицю метода handleAddToHead

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,19 @@
-import React, { FormEvent, useEffect, useMemo, useState } from "react";
+import React, {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { ArrowIcon } from "../ui/icons/arrow-icon";
 import { LinkedList } from "./linked-list";
+import { ElementStates } from "../../types/element-states";
+import { pause } from "../../utils/pause";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 import styles from "./linked-list.module.css";
 
 const INITIAL_LIST_ITEMS = ["0", "34", "8", "1"];
@@ -15,31 +24,54 @@ type TListItem = {
 };
 
 export const ListPage: React.FC = () => {
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
+  const [activeIndex, setActiveIndex] = useState<number | undefined>();
   const [inputValue, setInputValue] = useState<string>("");
   const [indexValue, setIndexValue] = useState<string>("");
   const [listItems, setListItems] = useState<TListItem[]>();
   const list = useMemo(() => new LinkedList<string>(), []);
 
-  const getListItems = () => {
-    return list.getItems().map((item) => ({
-      letter: item,
-    }));
-  };
+  // При генерации элементов можем указать индекс и стейт элемента для подсветки.
+  const getListItems = useCallback(
+    (i?: number, status?: ElementStates) => {
+      return list.getItems().map((item, index) => ({
+        letter: item,
+        state: i === index ? status : ElementStates.Default,
+      }));
+    },
+    [list]
+  );
 
   useEffect(() => {
     INITIAL_LIST_ITEMS.forEach((item) => list.append(item));
     setListItems(getListItems());
-  }, [list]);
+  }, [list, getListItems]);
 
-  const handleAddToHead = () => {
+  const handleAddToHead = async () => {
     if (!inputValue) return;
-    console.log("appending to head", inputValue);
+    // Начинаем анимацию
+    setIsAnimating(true);
+    // Подсвечиваем 1-й элемент, устанвливаем статус "adding", делаем паузу
+    setIsAdding(true);
+    setActiveIndex(0);
+    await pause(SHORT_DELAY_IN_MS);
+    // После паузы убираем подсветку
+    setIsAdding(false);
+    setActiveIndex(undefined);
+    // Обновляем список и подсвечиваем измененный элемент, делаем паузу
     list.prepend(inputValue);
+    setListItems(getListItems(0, ElementStates.Modified));
+    await pause(SHORT_DELAY_IN_MS);
+    // После паузы ставим всем элементам стейт "Default"
     setListItems(getListItems());
+    // Выключаем анимацию и очищаем инпут
+    setIsAnimating(false);
+    setInputValue("");
   };
 
-  const handleAddToTail = () => {
+  const handleAddToTail = async () => {
     if (!inputValue) return;
     console.log("appending to tail", inputValue);
     list.append(inputValue);
@@ -47,6 +79,8 @@ export const ListPage: React.FC = () => {
   };
 
   const handleDeleteFromHead = () => {
+    setIsAnimating(true);
+    setIsDeleting(true);
     console.log("delete from head");
     list.deleteAt(0);
     setListItems(getListItems());
@@ -91,7 +125,6 @@ export const ListPage: React.FC = () => {
           value={inputValue}
           maxLength={4}
           isLimitText={true}
-          disabled={isAnimating}
           onChange={(evt: FormEvent<HTMLInputElement>) =>
             setInputValue((evt.target as HTMLInputElement).value)
           }
@@ -149,12 +182,35 @@ export const ListPage: React.FC = () => {
           listItems.map((item, i) => {
             return (
               <li key={i} className={styles.listitem}>
+                {i === activeIndex && isAdding && isAnimating && (
+                  <Circle
+                    isSmall={true}
+                    extraClass={styles.top}
+                    letter={inputValue}
+                    state={ElementStates.Changing}
+                  />
+                )}
+
                 <Circle
                   index={i}
-                  head={i === 0 ? "head" : ""}
-                  tail={i === list.getSize() - 1 ? "tail" : ""}
+                  head={i === 0 && activeIndex !== 0 ? "head" : ""}
+                  tail={
+                    i === list.getSize() - 1 &&
+                    activeIndex !== list.getSize() - 1
+                      ? "tail"
+                      : ""
+                  }
                   {...item}
                 />
+
+                {i === activeIndex && isDeleting && isAnimating && (
+                  <Circle
+                    isSmall={true}
+                    extraClass={styles.bottom}
+                    letter={inputValue}
+                    state={ElementStates.Changing}
+                  />
+                )}
                 {i < list.getSize() - 1 && <ArrowIcon />}
               </li>
             );
