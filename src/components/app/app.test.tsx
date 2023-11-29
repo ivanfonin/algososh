2c2c3f659aa6e495aa0e036c0cf173b37b0a4ce2
Ivan Fonin <ivanfonin@yandex.ru>
2023-11-29 11:36:11 +0300

    refactor: Протестировать тест из коробки

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./app";
 
-test("renders learn react link", () => {
+test("renders app component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/МБОУ АЛГОСОШ/i);
+  expect(headingElement).toBeInTheDocument();
 });
