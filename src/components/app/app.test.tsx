e9c675468d25714f4bc30b2df39b1a988e944545
Ivan Fonin <ivanfonin@yandex.ru>
2023-12-09 23:55:30 +0300

    feat: Удалить тест App

--- src/components/app/app.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./app";
-
-test("renders app component", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/МБОУ АЛГОСОШ/i);
-  expect(headingElement).toBeInTheDocument();
-});
